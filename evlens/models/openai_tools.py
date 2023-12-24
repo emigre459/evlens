3e8e806656338c5ec28f9cb8af006c9decae3a65
Dave Rench McCauley <11720378+emigre459@users.noreply.github.com>
2023-12-23 23:32:55 -0500

    Add token counting of images

--- evlens/models/openai_tools.py
+++ evlens/models/openai_tools.py
@@ -1,5 +1,9 @@
-from typing import List, Union
+from typing import List, Union, Tuple
+from math import ceil
 from openai import OpenAI
+from PIL import Image
+import io
+import requests
 
 
 def find_models(
@@ -19,3 +23,42 @@ def find_models(
         return None
     else:
         return models_found
+    
+
+# From https://stackoverflow.com/a/44481279/8630238
+def url_to_image(url: str) -> Image:
+    data = requests.get(url).content
+    return Image.open(io.BytesIO(data))
+
+
+# Adapted from https://community.openai.com/t/how-do-i-calculate-image-tokens-in-gpt4-vision/492318/2
+def count_image_tokens(
+    dimensions: Tuple[int, int] = None,
+    img: Image = None,
+    url: str = None,
+    high_resolution: bool = True
+) -> int:
+    
+    if dimensions is not None:
+        height, width = dimensions
+    elif img is not None:
+        height = img.height
+        width = img.width
+    elif url is not None:
+        img = url_to_image(url)
+        height = img.height
+        width = img.width
+    else:
+        raise ValueError("One of `dimensions`, `object`, or `url` must be "
+                         "provided")
+    
+    if high_resolution:
+        h = ceil(height / 512)
+        w = ceil(width / 512)
+        n = w * h
+        total = 85 + 170 * n
+        return total
+    
+    else:
+        return 85 # default tokens for low-res
+    
