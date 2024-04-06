6beb73ae87a77320cd4fc5133674ed369b6e4a45
drad2042 <76827488+drad2042@users.noreply.github.com>
2024-04-05 23:08:25 -0400

    Included comments, still working through using nearby locations to detect only American stations.

--- scripts/plugshareTest.py
+++ scripts/plugshareTest.py
@@ -13,111 +13,116 @@ class Scraper:
         self.chrome_options.add_argument("--disable-infobars")
         self.chrome_options.add_argument("--disable-extensions")
         self.chrome_options.add_argument("--disable-notifications")
-        self.prefs = {"profile.default_content_setting_values.geolocation" :2} #TURN OFF LOCATION!!! (NOT NECESSARY BUT LESS TIME NEEDED)
+        self.prefs = {"profile.default_content_setting_values.geolocation":2} #TURN OFF LOCATION!!! (NOT NECESSARY BUT LESS TIME NEEDED)
         self.chrome_options.add_experimental_option("prefs", self.prefs)
         #chrome_options.add_experimental_option("detach", True)
 
-    
-    def scrape_plugshare_locations(self, start_location, end_location):
-        
-        driver = webdriver.Chrome(options=self.chrome_options) # Open connection!
-        all_stations = []
-
-        for location_id in range(start_location, end_location):
-            try:
-                url = f"https://www.plugshare.com/location/{location_id}" #DESIRED URL
-                driver.get(url)
-            except:
-                continue
-
-            time.sleep(3)  # Allow time for the page to load!!
-
-            try:
-                login = driver.find_element(By.XPATH, "//*[@id=\"dialogContent_authenticate\"]/div[2]/div[2]/span")
-                login.click()
+    def plugshare_login(self):
+        try:
+            login = self.driver.find_element(By.XPATH, "//*[@id=\"dialogContent_authenticate\"]/div[2]/div[2]/span")
+            login.click()
                 
-                email = driver.find_element(By.XPATH, "//*[@id=\"email\"]")
-                email.send_keys("kingdan017@gmail.com")
+            email = self.driver.find_element(By.XPATH, "//*[@id=\"email\"]")
+            email.send_keys("kingdan017@gmail.com")
                 
-                password = driver.find_element(By.XPATH, "//*[@id=\"input_259\"]")
-                password.send_keys("PlugShareTemp1")
+            password = self.driver.find_element(By.XPATH, "//*[@id=\"input_259\"]")
+            password.send_keys("PlugShareTemp1")
                 
-                final_login = driver.find_element(By.XPATH, "//*[@id=\"auth-form\"]/md-content/div[5]/button")
-                final_login.click()
+            final_login = self.driver.find_element(By.XPATH, "//*[@id=\"auth-form\"]/md-content/div[5]/button")
+            final_login.click()
                 
                 #esc_button = driver.find_element(By.XPATH, "//*[@id=\"dialogContent_authenticate\"]/button/md-icon")
                 #esc_button.click()
-            except:
-                pass
+        except:
+            pass
+    
+    def data_scrape(self):
+        try: ## FIND STATION NAME
+            self.name = self.driver.find_element(By.XPATH, "//*[@id=\"display-name\"]/div/h1").text
+        except:
+            self.name = "NA"
+        
+        try: ## FIND STATION ADDRESS
+            self.address = self.driver.find_element(By.XPATH, "//*[@id=\"info\"]/div[2]/div[3]/div[2]/a[1]").text
+        except:
+            self.address = "NA"
+        
+        try: ## FIND STATION RATING
+            self.rating = self.driver.find_element(By.XPATH, "//*[@id=\"plugscore\"]").text
+        except:
+            self.rating = "NA"
 
-            try: ## FIND STATION NAME
-                name = driver.find_element(By.XPATH, "//*[@id=\"display-name\"]/div/h1").text
-            except:
-                name = "NA"
+        try: ## FIND STATION WATTAGE
+            self.wattage = self.driver.find_element(By.XPATH, "//*[@id=\"ports\"]/div[3]").text
+        except:
+            self.wattage = "NA"
             
-            try: ## FIND STATION ADDRESS
-                address = driver.find_element(By.XPATH, "//*[@id=\"info\"]/div[2]/div[3]/div[2]/a[1]").text
-            except:
-                address = "NA"
-            
-            try: ## FIND STATION RATING
-                rating = driver.find_element(By.XPATH, "//*[@id=\"plugscore\"]").text
-            except:
-                rating = "NA"
+        try: ## FIND STATION HOURS
+            self.hours = self.driver.find_element(By.XPATH, "//*[@id=\"info\"]/div[2]/div[11]/div[2]/div").text
+        except:
+            self.hours = "NA"
 
-            try: ## FIND STATION WATTAGE
-                wattage = driver.find_element(By.XPATH, "//*[@id=\"ports\"]/div[3]").text
-            except:
-                wattage = "NA"
+        try: # FIND CHECKINS
+            self.checkins = self.driver.find_element(By.XPATH, "//*[@id=\"checkins\"]").text
+            self.checkins = self.checkins.split(' ')[1].split('\n')[0]
+            #checkins = checkins.split('\n')[0]
             
-            try: ## FIND STATION HOURS
-                hours = driver.find_element(By.XPATH, "//*[@id=\"info\"]/div[2]/div[11]/div[2]/div").text
-            except:
-                hours = "NA"
+            #self.comments = self.driver.find_elements(By.CLASS_NAME, "comment ng-binding basic")
+            #for comment in self.comments:
+            #    print(comment.text)
+                #print(comment.text.replace("check_circle",""))
+
+        except:
+            pass
+
+        try:
+            self.nearby_locations = self.driver.find_elements(By.CLASS_NAME, "location ng-scope mapreact")
+            print(self.nearby_locations + "TOTAL NEARBY LOCATIONS LIST")
+            for location in self.nearby_locations:
+                print(location.text + "NEARBY LOCATIONS")
+
+        except:
+            pass
+            
+        try:
+            self.cars = self.driver.find_elements(By.CLASS_NAME, "car ng-binding") # PRINTS TYPE OF CAR FOR EACH PERSON
+            for car in self.cars:
+                print(car)
+            
+        except:
+            pass
+            
+        try:
+            self.all_stations.append({"Name": self.name, "Address": self.address, "Rating": self.rating, "Wattage": self.wattage, "Hours": self.hours, "Checkins": self.checkins})
+        except:
+            pass
+        
+    def scrape_plugshare_locations(self, start_location, end_location):
+        self.locationlist = []
+        self.driver = webdriver.Chrome(options=self.chrome_options) # Open connection!
+        self.all_stations = []
+
+        for location_id in range(start_location, end_location):
+            self.locationlist.append(location_id)
 
             try:
-                checkins = driver.find_element(By.XPATH, "//*[@id=\"checkins\"]").text
-                checkins = checkins.split(' ')[1].split('\n')[0]
-                #checkins = checkins.split('\n')[0]
-                comments = driver.find_elements(By.CLASS_NAME, "user")
-                cars = driver.find_elements(By.CLASS_NAME, "car ng-binding")
-
-                for comment in comments:
-                    #print(comment.text)
-                    print(comment.text.replace("check_circle",""))
-
-                for car in cars:
-                    print(car)
-                    
+                url = f"https://www.plugshare.com/location/{location_id}" #DESIRED URL
+                self.driver.get(url)
             except:
-                checkins = "NA"
-
-            try:
-                nearby_locations = driver.find_elements(By.CLASS_NAME, "location ng-scope mapreact")
-                for location in nearby_locations:
-                    print(location.text)
-
-            except:
-                nearby_locations = "NA"
-            
-            try:
-                score = driver.find_element(By.XPATH, "//*[@id=\"plugscore\"]").text
-            except:
-                score = "NA"
-
-            if (name and address and rating and wattage and hours) == "NA":
                 continue
+
             
-            try:
-                all_stations.append({"Name": name, "Address": address, "Rating": rating, "Wattage": wattage, "Hours": hours, "Checkins": checkins, score: "Score"})
-            except:
-                pass
+            time.sleep(3)  # Allow time for the page to load!!
+
+            self.plugshare_login()
+            self.data_scrape()
 
             time.sleep(1)
 
 
-        driver.quit()
-        df = pd.DataFrame(all_stations)
+
+        self.driver.quit()
+        df = pd.DataFrame(self.all_stations)
         return df 
     
 #RUN THE FUNCTION!
@@ -126,7 +131,7 @@ s = Scraper()
 
 start = time.time()
 
-caller = s.scrape_plugshare_locations(100000,100015)
+caller = s.scrape_plugshare_locations(130987,140000)
 caller.to_csv('Plugshare.csv', index = False)
 print(caller)
 
