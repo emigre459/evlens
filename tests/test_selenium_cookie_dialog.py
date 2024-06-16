e95aa0b495f400d19fd1654d78889c523bd55613
Dave Rench McCauley <11720378+emigre459@users.noreply.github.com>
2024-06-16 14:27:40 -0400

    Add option to accept cookies in test

--- tests/test_selenium_cookie_dialog.py
+++ tests/test_selenium_cookie_dialog.py
@@ -14,13 +14,15 @@ URL = f"https://www.plugshare.com/location/{TEST_LOCATION}"
 
 chrome_options = Options()
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
 DRIVER = webdriver.Chrome(options=chrome_options)
+
 
 
 # Add a cookie to the browser that indicates that the user has not consented to cookies
 # driver.add_cookie({"name": "consent", "value": "false"})
 
-def reject_cookies(driver, url):
+def interact_with_cookies(driver, url, reject_cookies: bool = True):
     """Rejects cookies on a website using Selenium.
 
     Args:
@@ -34,54 +36,63 @@ def reject_cookies(driver, url):
     driver.get(url)
 
     try:
-        wait = WebDriverWait(driver, 2)
+        wait = WebDriverWait(driver, 5)
 
         # Wait for the cookie dialog to appear
-        wait.until(EC.visibility_of_element_located((
+        iframe = wait.until(EC.visibility_of_element_located((
             By.ID,
-            "global-consent-tool-wrapper"
+            "global-consent-notice"
         )))
+        
+        logger.info("Found the banner!")
         
         # Select default content so we can switch to cookie iframe
         # Adapted from https://stackoverflow.com/a/21476147
         # Pull out of main page frame so we can select a different frame (cookies)
-        driver.switch_to.default_content()
-        driver.switch_to.frame(driver.find_element(By.ID, "global-consent-notice"))
-        manage_settings_link = driver.find_element(By.LINK_TEXT, "Manage Settings")
+        # driver.switch_to.default_content()
+        driver.switch_to.frame(iframe)
         
-        # Click the link
-        manage_settings_link.click()
-        
-        
-        # Switch to cookie frame
+        if reject_cookies:
+            logger.info("Rejecting cookies...")
+            manage_settings_link = driver.find_element(By.LINK_TEXT, "Manage Settings")
+            
+            
+            # Click the link
+            manage_settings_link.click()
+            
+        else:
+            logger.info("Accepting cookies...")
+            accept_button = driver.find_element(By.TAG_NAME, "button")
+            accept_button.click()
         
         
         
         # Switch back to main frame
-        
+        driver.switch_to.default_content()
         
 
         return True
 
-    except (NoSuchElementException, TimeoutException):
-        print("Cookie banner or 'Manage Settings' link not found. Assuming cookies are not rejected.")
-        return False
+    except (NoSuchElementException, TimeoutException) as e1:
+        logger.error("Cookie banner or 'Manage Settings' link not found. Assuming cookies are not rejected.")
+        raise e1
 
-    except Exception as e:
-        print(f"Unknown rror rejecting cookies: {e}")
-        return False
+    except Exception as e2:
+        logger.error(f"Unknown error rejecting cookies: {e}")
 
-# Example usage
-assert reject_cookies(DRIVER, URL), "Error rejecting cookies."
-DRIVER.get(URL)
+if __name__ == '__main__':
+    # Example usage
+    logger.info("Starting cookie dialog test...")
+    assert interact_with_cookies(DRIVER, URL, reject_cookies=False), "Error rejecting cookies."
 
-# Refresh the page to see the "Manage Settings" link
-# driver.refresh()
+    # Refresh the page to see the "Manage Settings" link
+    # driver.refresh()
 
-print("SUCCESS!")
+    logger.info("SUCCESS!")
+    # DRIVER.switch_to.frame(iframe)
 
 
-# Ultimately flow using this code should be:
-# 1. Check if banner is present via try-except on timeout
-# 2. If present, go through rejection logic then exit login dialog
-# 3. If missing, proceed with exiting login dialog directly
+    # Ultimately flow using this code should be:
+    # 1. Check if banner is present via try-except on timeout
+    # 2. If present, go through rejection logic then exit login dialog
+    # 3. If missing, proceed with exiting login dialog directly
