d97db6952eda3ab8be12fdf4212b98aff5632956
Dave Rench McCauley <11720378+emigre459@users.noreply.github.com>
2024-06-18 14:08:34 -0400

    Adjust code for using Checkin object and capturing more checkins per location

--- evlens/data/plugshare.py
+++ evlens/data/plugshare.py
@@ -3,15 +3,80 @@ from tqdm import tqdm
 import pandas as pd
 import numpy as np
 import os
+import re
+from typing import Tuple
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.remote.webelement import WebElement
 
 from evlens.logs import setup_logger
 logger = setup_logger(__name__)
+
+
+class CheckIn:
+    '''
+    Tracks all the different components of a single check-in and can return as a single-row pandas DataFrame to be used elsewhere.
+    '''
+    def __init__(self, checkin_element: WebElement):
+        self.element = checkin_element
+        
+    def parse(self) -> pd.DataFrame:
+        '''
+        Parses the checkin_element provided during instantiation and returns
+        the info found.
+
+        Returns
+        -------
+        pd.DataFrame
+            Single-row DataFrame providing the data comprising the check-in
+        '''
+        output = dict()
+        
+        # Details part
+        details_element = self.element.find_element(By.CLASS_NAME, "details")
+        details_children = details_element.find_elements(By.XPATH, "./child::*")
+        for d in details_children:
+            if d.get_attribute("class") == 'date ng-binding':
+                output['date'] = pd.to_datetime(d.text)
+            elif d.get_attribute("class") == 'car ng-binding':
+                output['car'] = d.text
+            elif d.get_attribute("class") == 'additional':
+                self.additional_children = d.find_elements(By.XPATH, "./child::*")
+        
+        # "Additional" part
+        print([d.get_attribute("class") for d in self.additional_children])
+        for d in self.additional_children:
+            if d.get_attribute("class") == 'problem ng-scope':
+                output['problem'] = d.text
+            elif d.get_attribute("class") == 'connector ng-binding':
+                output['connector_type'] = d.text
+            elif d.get_attribute("class") == 'kilowatts ng-scope':
+                output['charge_power_kilowatts'] = d.text
+            elif d.get_attribute("class") == 'comment ng-binding':
+                output['comment'] = d.text
+                
+        
+        # Check what columns we're missing and fill with null
+        expected_columns = [
+            'date',
+            'car',
+            'problem',
+            'connector_type',
+            'charge_power_kilowatts',
+            'comment'
+        ]
+        for c in expected_columns:
+            if c not in output.keys():
+                output[c] = np.nan
+        
+        
+        return pd.DataFrame(output, index=[0])
+
 
 class Scraper:
 
@@ -54,7 +119,6 @@ class Scraper:
         
         #TODO: get rid of these through refactor
         self.locationlist = []
-        self.all_stations = []
         
     def exit_login_dialog(self):
         logger.info("Attempting to exit login dialog...")
@@ -115,94 +179,115 @@ class Scraper:
     
     #TODO: clean up and try to more elegantly extract things en masse
     #TODO: use fewer attributes for data maintenance
-    def scrape_location(self):
+    def scrape_location(self, location_id: int) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        '''
+        Scrapes a single location (single webpage)
+
+        Returns
+        -------
+        Tuple[pd.DataFrame, pd.DataFrame]
+            (Single-row dataframe with location metadata, dataframe with one row per check-in comment)
+        '''
+        output = dict()
+        
         logger.info("Starting page scrape...")
         try: ## FIND STATION NAME
-            self.wait.until(EC.visibility_of_element_located((
+            output['name'] = self.wait.until(EC.visibility_of_element_located((
                 By.XPATH,
                 "//*[@id=\"display-name\"]/div/h1"
-            )))
-            self.name = self.driver.find_element(
-                By.XPATH,
-                "//*[@id=\"display-name\"]/div/h1"
-                ).text
+            ))).text
+            
         except (NoSuchElementException, TimeoutException):
             logger.error("Station name error, skipping...")
             return
         
         try: ## FIND STATION ADDRESS
             
-            self.address = self.driver.find_element(By.XPATH, "//*[@id=\"info\"]/div[2]/div[3]/div[2]/a[1]").text
+            output['address'] = self.driver.find_element(By.XPATH, "//*[@id=\"info\"]/div[2]/div[3]/div[2]/a[1]").text
+            
         except (NoSuchElementException, TimeoutException):
             logger.error("Station address error", exc_info=True)
-            self.address = np.nan
+            output['address'] = np.nan
         
         try: ## FIND STATION RATING
             
-            self.rating = self.driver.find_element(By.XPATH, "//*[@id=\"plugscore\"]").text
+            output['plugscore'] = self.driver.find_element(By.XPATH, "//*[@id=\"plugscore\"]").text
+            
         except (NoSuchElementException, TimeoutException):
             logger.error("Station rating error", exc_info=True)
-            self.rating = np.nan
+            output['plugscore'] = np.nan
 
         try: ## FIND STATION WATTAGE
             
-            self.wattage = self.driver.find_element(By.XPATH, "//*[@id=\"ports\"]/div[3]").text
+            output['wattage'] = self.driver.find_element(By.XPATH, "//*[@id=\"ports\"]/div[3]").text
+            
         except (NoSuchElementException, TimeoutException):
             logger.error("Wattage error", exc_info=True)
-            self.wattage = np.nan
+            output['wattage'] = np.nan
             
         try: ## FIND STATION HOURS
             
-            self.hours = self.driver.find_element(By.XPATH, "//*[@id=\"info\"]/div[2]/div[11]/div[2]/div").text
+            output['service_hours'] = self.driver.find_element(By.XPATH, "//*[@id=\"info\"]/div[2]/div[11]/div[2]/div").text
+            
         except (NoSuchElementException, TimeoutException):
             logger.error("Station hours error", exc_info=True)
-            self.hours = np.nan
+            output['service_hours'] = np.nan
 
-        try: # FIND CHECKINS 
+        try: # Get total check-in counts
+            def _get_checkin_count(text):
+                match = re.search(r"\(\s*(\d+)\s*\)", text)
+                if match:
+                    return int(match.group(1))
+                return np.nan
             
-            self.checkins = self.driver.find_element(By.XPATH, "//*[@id=\"checkins\"]").text
-            self.checkins = self.checkins.split(' ')[1].split('\n')[0]
-            #checkins = checkins.split('\n')[0]
+            checkins = self.driver.find_element(
+                By.XPATH,
+                "//*[@id=\"checkins\"]"
+                ).text
+            output['checkin_count'] = _get_checkin_count(checkins)
+            
         except (NoSuchElementException, TimeoutException):
-            logger.error("Check-ins error", exc_info=True)
+            logger.error("Check-in count error", exc_info=True)
+            output['checkin_count'] = np.nan
 
-        #TODO: try to pull more comments, might need clicking to View More
-        #TODO: better parse the comments, come out as str but need list
-        try: # FIND COMMENTS
+        try: # PULL IN COMMENT TEXT
+            more_comments_link = self.driver.find_element(
+                By.XPATH,
+                "//*[@id=\"checkins\"]/div[2]/span[3]"
+            )
+            more_comments_link.click()
             
-            self.commentList = []
-            self.comments = self.driver.find_elements(By.CLASS_NAME, "details")
-            for comment in self.comments:
-                self.commentList.append(comment.text)
-             
-            for comment in self.commentList:
-                comment.replace("check_circle", "")
-            self.finalComments = ', '.join(self.commentList)
+            detailed_checkins = self.driver.find_element(
+                By.XPATH,
+                "//*[@id=\"dialogContent_reviews\"]/div/div"
+            ).find_elements(By.XPATH, "./child::*")
+            
+            checkin_dfs = []
+            for checkin in detailed_checkins:
+                c = CheckIn(checkin)
+                checkin_dfs.append(c.parse())
+                
+            df_checkins = pd.concat(checkin_dfs, ignore_index=True)
+            df_checkins['location_id'] = location_id
+            
         except (NoSuchElementException, TimeoutException):
             logger.error("Comments error", exc_info=True)
             
-        try: # SCRAPE CAR
-            
-            carList = []
-            self.cars = self.driver.find_elements(By.CLASS_NAME, "car ng-binding") # PRINTS TYPE OF CAR FOR EACH PERSON
-            for car in self.cars:
-                carList.append(car)
-            self.cars = ', '.join(carList)
-        except (NoSuchElementException, TimeoutException):
-            logger.error("Car details error", exc_info=True)
-            
-        try: # PUT ALL TOGETHER
-            
-            self.all_stations.append({"Name": self.name, "Address": self.address, "Rating": self.rating, "Wattage": self.wattage, "Hours": self.hours, "Checkins": self.checkins, "Comments": self.finalComments, "Car": self.cars})
-        except:
-            logger.error("Data append error", exc_info=True)
-            
         logger.info("Page scrape complete!")
+        df_location = pd.DataFrame(output, index=[0])
+        df_location['id'] = location_id
+        
+        return (
+            df_location,
+            df_checkins
+        )
         
     #TODO: add in saving to disk at X pages (pickle for now)
     def run(self, start_location, end_location):
         logger.info("Beginning scraping!")
 
+        all_locations = []
+        all_checkins = []
         for i, location_id in enumerate(tqdm(
             range(start_location, end_location+1),
             desc="Parsing stations"
@@ -222,12 +307,18 @@ class Scraper:
                 
             # TODO: try-except here
             self.exit_login_dialog()
-            self.scrape_location()
+            df_location, df_checkins = self.scrape_location(location_id)
+            all_locations.append(df_location)
+            all_checkins.append(df_checkins)
             
             if i+1 % self.save_every == 0:
                 logger.info(f"Saving checkpoint at location {i}")
-                path = self.save_path + f"{i}.pkl"
-                pd.DataFrame(self.all_stations).to_pickle(path)
+                
+                path = self.save_path + f"df_locations_{i}.pkl"
+                pd.concat(all_locations, ignore_index=True).to_pickle(path)
+                
+                path = self.save_path + f"df_checkins_{i}.pkl"
+                pd.concat(all_checkins, ignore_index=True).to_pickle(path)
 
             #TODO: tune between page switches
             logger.info(f"Sleeping for {self.page_load_pause} seconds")
@@ -236,7 +327,11 @@ class Scraper:
         self.driver.quit()
         
         #TODO: add station location integers as column
-        df = pd.DataFrame(self.all_stations)
-        df.to_pickle(self.save_path + f"all_data.pkl")
+        df_all_locations = pd.concat(all_locations, ignore_index=True)
+        df_all_locations.to_pickle(self.save_path + f"df_all_locations.pkl")
+        
+        df_all_checkins = pd.concat(all_checkins, ignore_index=True)
+        df_all_checkins.to_pickle(self.save_path + f"df_all_checkins.pkl")
+        
         logger.info("Scraping complete!")
-        return df
+        return df_all_locations, df_all_checkins
