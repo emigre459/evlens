873101cfb1d46659960f9ed9c6bce3956b502db9
Dave Rench McCauley <11720378+emigre459@users.noreply.github.com>
2024-06-24 13:05:27 -0400

    Add test coords to scraper to tune calibration of timeouts, trying to reduce scrape time for location grabbing
    Will need to remove test coords once we're happy with our tests

--- scripts/scrape_location_ids.py
+++ scripts/scrape_location_ids.py
@@ -10,13 +10,18 @@ logger = setup_logger(__name__)
 from datetime import date
 TODAY_STRING = date.today().strftime("%m-%d-%Y")
 
-SLEEP_FOR_IFRAME_PAN = 1.5
+SELENIUM_WAIT_TIME = 3
+SLEEP_FOR_IFRAME_PAN = 1.5 # this has been tested but not in headless mode, maybe can go faster?
+
+TEST_COORDS = (40.7525834,-73.9999498) # Lat, long
+RADIUS = 1 # miles
 
 
+#TODO: tune how long we need to sleep and timeout
 if __name__ == '__main__':
     lis = LocationIDScraper(
         f"./data/external/plugshare/{TODAY_STRING}/",
-        timeout=3,
+        timeout=SELENIUM_WAIT_TIME,
         headless=True
     )
     
@@ -25,7 +30,15 @@ if __name__ == '__main__':
     df_map_tiles = pd.read_pickle('references/h3_hexagon_coordinates.pkl')
     
     #TODO: build in functionality for starting from a checkpoint file i+1
-    criteria = []
+    # criteria = []
+    # Make sure we have some pins we know should be good
+    # Expect location IDs [563873, 574882]
+    criteria = [SearchCriterion(
+        TEST_COORDS[0],
+        TEST_COORDS[1],
+        RADIUS,
+        SLEEP_FOR_IFRAME_PAN
+    )]
     for _, row in tqdm(
         df_map_tiles.iterrows(),
         desc='Building search criteria from gridded map',
